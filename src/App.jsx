a8be292d5f9350139f509fa1cbf3a1349e712bfc
Cristian Rodriguez <cristian1993na@hotmai.com>
2025-11-26 23:07:14 -0500

    Fix: corrected Login.jsx path for Linux

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,28 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Páginas
-import Home from "./pages/Home";
-import Search from "./pages/Search";
-import KidsZone from "./pages/kidsZone";
-import Login from "./pages/login";
-import Profile from "./pages/profile";
+import Home from "./pages/Home.jsx";
+import Search from "./pages/Search.jsx";
+import KidsZone from "./pages/kidsZone.jsx";
+import Login from "./pages/login.jsx";
+import Profile from "./pages/profile.jsx";
 
 // Juegos
-import AirplaneGame from "./pages/game/AirplaneGame";
-import RocketGame from "./pages/game/RocketGame";
-import KidsPuzzle from "./pages/game/KidsPuzzle";
+import AirplaneGame from "./pages/game/AirplaneGame.jsx";
+import RocketGame from "./pages/game/RocketGame.jsx";
+import KidsPuzzle from "./pages/game/KidsPuzzle.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Search" element={<Search />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/kids" element={<KidsZone />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
 
+        {/* Juegos */}
         <Route path="/kids/airplane-game" element={<AirplaneGame />} />
         <Route path="/kids/rocket-game" element={<RocketGame />} />
         <Route path="/kids/puzzle" element={<KidsPuzzle />} />
@@ -32,4 +33,5 @@ function App() {
 }
 
 export default App;
+
 
